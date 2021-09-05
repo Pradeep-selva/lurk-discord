3bfd753ead8686c3fa2082079ad0c1b7e9700d3f
pradeep-selva <pradeepselvaraju@gmail.com>
2021-09-05 22:49:00 +0530

    feature: lurk/text indicators

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,45 @@
+const Assets = {
+  openEye: "../assets/eye-open.png",
+  closedEye: "../assets/eye-shut.png"
+};
+
+const createBtnAccessory = (assetSource = Assets.openEye) => {
+  try {
+    const accessoryPlaceholder = document.querySelector(
+      "#btn-accessory-placeholder"
+    );
+    const prevBtnAccessory = document.querySelector("#btn-accessory");
+
+    if (!!prevBtnAccessory) {
+      prevBtnAccessory.remove();
+    }
+
+    const btnAccessory = document.createElement("img");
+    btnAccessory.setAttribute("id", "btn-accessory");
+    btnAccessory.setAttribute("alt", "indicator-icon");
+    btnAccessory.setAttribute("src", assetSource);
+
+    accessoryPlaceholder.appendChild(btnAccessory);
+  } catch (e) {
+    console.log("[LURK DISCORD] Error: ", e);
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  let triggerButton = document.querySelector("#message");
+  createBtnAccessory();
   let isLurking = false;
+
+  let triggerButton = document.querySelector("#btn-message");
+  let triggerButtonText = document.querySelector("#btn-message-text");
 
   triggerButton.addEventListener("click", () => {
     if (!isLurking) {
-      triggerButton.innerHTML = "<b>stop lurking</b>";
+      triggerButtonText.innerText = "stop lurking";
+      createBtnAccessory(Assets.closedEye);
       isLurking = true;
     } else {
-      triggerButton.innerHTML = "<b>start lurking</b>";
+      triggerButtonText.innerText = "start lurking";
+      createBtnAccessory(Assets.openEye);
       isLurking = false;
     }
 
